b9caf100d63b6cdaee6936599d474d26b93af617
salt_wizard <salt_wizard>
2024-01-27 12:35:57 -0600

    Initial commit; Creating project skeleton to be used with Streamer.Bot

---
+++ RaidSelector/Program.cs
@@ -0,0 +1,50 @@
+﻿#pragma warning disable CA1822, CA1050
+using CPHNameSpace;                // Mimic CPH for method references
+using static CPHNameSpace.CPHArgs; // Mimic arguments for inline CPH
+
+
+/************************************************************************
+* COPY AND PASTE BELOW CLASS INTO STREAMER.BOT 
+* DO NOT COPY ANYTHING OUTSIDE THE BLOCK COMMENT
+************************************************************************/
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+public class CPHInline
+{
+
+    public void Init()
+    {
+        // place your init code here
+    }
+
+    public void Dispose()
+    {
+        // place your dispose code here
+    }
+
+    public bool Execute()
+    {
+        // place your main code here
+
+        return true;
+    }
+
+    /** 
+     * Custom methods go below 
+     * 
+     * Any method that is to be used by Streamer.Bot must have a reutrn type of bool and no parameters
+     * **/
+
+
+    public string ObjToString(object obj)
+    {
+        return JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonConverter[] { new StringEnumConverter() });
+    }
+}
+/************************************************************************
+* COPY AND PASTE ABOVE CLASS INTO STREAMER.BOT
+* DO NOT COPY ANYTHING OUTSIDE THE BLOCK COMMENT
+************************************************************************/
+#pragma warning restore CA1822, CA1050
---
+++ RaidSelector/cphargs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CPHNameSpace
+{
+    public static class CPHArgs
+    {
+#pragma warning disable CS8618, CA2211
+        public static Dictionary<string, object> args;
+#pragma warning restore CS8618, CA2211
+    }
+}
